c9e5ea5736870beb825e2c4f4bdd77cf2772c312
AthillaZaidan <athillazaidanstudy@gmail.com>
2025-11-07 22:30:49 +0700

    feat: rapihkan layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -16,18 +14,15 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Aethera",
   description: "Weather App with Intelligence",
+  icons: { icon: "/favicon.png" }, // Just put favicon.png in public
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
